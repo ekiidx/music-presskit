87ae9d75d132245ccb10e350ec54c081dc7148a7
ekiidx <info@vuedesign.co>
2023-09-11 23:39:34 -0400

    uploaded assets

--- index.php
+++ index.php
@@ -1,4 +1,54 @@
 <?php include 'header.php'; ?>
 
+<section id="hero-main">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <span class="d-block" style="color:#ffffff;">Hello</span>
+            </div>
+        </div>
+    </div>
+</section>
 
-<?php include 'footer.php'; 
+<section id="hero-music">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <span class="d-block" style="color:#ffffff;">Music</span>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="follow">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-6">
+                <span class="d-block">Follow Me</span>
+            </div>
+
+            <div class="col-6">
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Soundcloud</li>
+                    <li>Bandcamp</li>
+                    <li>Spotify</li>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                Contact
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php';

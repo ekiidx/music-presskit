ec12e291bbbed45aa25c99d19ac9a2def47edd74
ekiidx <info@vuedesign.co>
2023-09-12 15:41:38 -0400

    dashboard added

--- index.php
+++ index.php
@@ -4,17 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <span class="d-block" style="color:#ffffff;">Hello</span>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section id="hero-music">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <span class="d-block" style="color:#ffffff;">Music</span>
+                <h2>Hello</h2>
             </div>
         </div>
     </div>
@@ -22,26 +12,87 @@
 
 <section id="bio">
     <div class="container">
-        <div class="col-6">
-        </div>
+        <div class="row">
+            <div class="col-6">
+                <h2>GuyRo</h2>
 
-        <div class="col-6">
-            <p>Irish-born, Spanish-based electronica DJ and producer, David, a.k.a. GuyRo got swept up by the electronica steam train back during it's early beginnings. Harking back to his younger years, with fond memories as a pre-teen, armed with just a Sony Walkman and a collection of acid house mix tapes in his backpack, it is fair to say not many discover their passions quite as early as GuyRo. It wasn't long into his teenage years before he'd develop an insatiable thirst for crate digging — flipping through record after record picking out the dancefloor gems that would further ignite his passion for music.</p>
+                <p>Irish-born, Spanish-based electronica DJ and producer, David, a.k.a. GuyRo got swept up by the electronica steam train back during it's early beginnings. Harking back to his younger years, with fond memories as a pre-teen, armed with just a Sony Walkman and a collection of acid house mix tapes in his backpack, it is fair to say not many discover their passions quite as early as GuyRo. It wasn't long into his teenage years before he'd develop an insatiable thirst for crate digging — flipping through record after record picking out the dancefloor gems that would further ignite his passion for music.</p>
 
-            <p>With an open mind and fierce curiosity for anything and everything electronic music-based, he has spent the past two decades soaking up a multitude of genres and styles ranging from Detroit techno, breaks, house, trance, drum & bass and hip-hop. Artists like 808 State, Blake Baxter, Public Enemy, Alex Reece, Photek and BT are just some of the musicians that went on to shape GuyRo into the tastemaker he is today.</p>
+                <p>With an open mind and fierce curiosity for anything and everything electronic music-based, he has spent the past two decades soaking up a multitude of genres and styles ranging from Detroit techno, breaks, house, trance, drum & bass and hip-hop. Artists like 808 State, Blake Baxter, Public Enemy, Alex Reece, Photek and BT are just some of the musicians that went on to shape GuyRo into the tastemaker he is today.</p>
 
-            <p>Today, GuyRo focuses on pushing an infectious fusion of synth-laden techno and progressive house. With recent influences such as Danny Howells, LSG, Boston 168, Saeed & Palash, Sasha and Charlie May, you'll be hard-pressed to argue for a lack of diversity within his sets, which comprise of both his own productions and others. His tracks go from the deeper end of downtempo and breaks through to the tougher grooves of house and techno.</p>
+                <p>Today, GuyRo focuses on pushing an infectious fusion of synth-laden techno and progressive house. With recent influences such as Danny Howells, LSG, Boston 168, Saeed & Palash, Sasha and Charlie May, you'll be hard-pressed to argue for a lack of diversity within his sets, which comprise of both his own productions and others. His tracks go from the deeper end of downtempo and breaks through to the tougher grooves of house and techno.</p>
+            </div>
+            <div class="col-6">
+                <section id="photos">
+                    <h2>Photos</h2>
+                    
+                    <div class="row">
+                        <div class="col-4"><img src=""></div>
+                        <div class="col-4"><img src=""></div>
+                        <div class="col-4"><img src=""></div>
+                        <div class="col-4"><img src=""></div>
+                        <div class="col-4"><img src=""></div>
+                        <div class="col-4"><img src=""></div>
+                    </div>
+                </section>
+            </div>
         </div>
     </div>
-</div>
 </section>
+
+<section id="dashboard">
+    <div class="container">
+        <div class="row">
+
+        <div id="calendar" class="col-6">
+            <div class="container">
+                <h2>Calendar</h2>
+            </div>
+        </div>
+
+        <div id="music" class="col-6">
+            <div class="container">
+                <h2>Music</h2>
+            </div>
+        </div>
+
+        <div id="press" class="col-6">
+            <div class="container">
+                <h2>Press</h2>
+            </div>
+        </div>
+
+        <div id="socials" class="col-6">
+            <div class="container">
+                <h2>Social Activity</h2>
+                    <div class="row">
+                        <div class="col-6">
+                            <span class="d-block">Facebook: 2323</span>
+                            <span class="d-block">YouTube: 150</span>
+                            <span class="d-block">Soundcloud: 2399</span>
+                            <span class="d-block">Spotify: 11252</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="videos" class="col-6">
+            <div class="container">
+                <h2>Videos</h2>
+            </div>
+        </div>   
+
+    </div>
+</section>
+
+
 
 <section id="follow">
     <div class="container">
         <div class="row">
-
             <div class="col-6">
-                <span class="d-block">Follow Me</span>
+                <h2>Follow Me</h2>
             </div>
 
             <div class="col-6">
@@ -54,6 +105,7 @@
                     <li><img class="icon" src="/assets/img/spotify.svg">Spotify</li>
                     <li><img class="icon" src="/assets/img/youtube.svg">Spotify</li>
                     <li><img class="icon" src="/assets/img/linkedin.svg">Spotify</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -63,7 +115,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                Contact
+               <h2>Contact</h2>
             </div>
         </div>
     </div>

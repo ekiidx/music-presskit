99a74a3c01afe6b530e8c5b567d9e116949d7712
ekiidx <info@vuedesign.co>
2023-09-12 01:38:07 -0400

    header added

--- index.php
+++ index.php
@@ -20,6 +20,22 @@
     </div>
 </section>
 
+<section id="bio">
+    <div class="container">
+        <div class="col-6">
+        </div>
+
+        <div class="col-6">
+            <p>Irish-born, Spanish-based electronica DJ and producer, David, a.k.a. GuyRo got swept up by the electronica steam train back during it's early beginnings. Harking back to his younger years, with fond memories as a pre-teen, armed with just a Sony Walkman and a collection of acid house mix tapes in his backpack, it is fair to say not many discover their passions quite as early as GuyRo. It wasn't long into his teenage years before he'd develop an insatiable thirst for crate digging — flipping through record after record picking out the dancefloor gems that would further ignite his passion for music.</p>
+
+            <p>With an open mind and fierce curiosity for anything and everything electronic music-based, he has spent the past two decades soaking up a multitude of genres and styles ranging from Detroit techno, breaks, house, trance, drum & bass and hip-hop. Artists like 808 State, Blake Baxter, Public Enemy, Alex Reece, Photek and BT are just some of the musicians that went on to shape GuyRo into the tastemaker he is today.</p>
+
+            <p>Today, GuyRo focuses on pushing an infectious fusion of synth-laden techno and progressive house. With recent influences such as Danny Howells, LSG, Boston 168, Saeed & Palash, Sasha and Charlie May, you'll be hard-pressed to argue for a lack of diversity within his sets, which comprise of both his own productions and others. His tracks go from the deeper end of downtempo and breaks through to the tougher grooves of house and techno.</p>
+        </div>
+    </div>
+</div>
+</section>
+
 <section id="follow">
     <div class="container">
         <div class="row">
@@ -30,12 +46,14 @@
 
             <div class="col-6">
                 <ul>
-                    <li>Facebook</li>
-                    <li>Twitter</li>
-                    <li>Instagram</li>
-                    <li>Soundcloud</li>
-                    <li>Bandcamp</li>
-                    <li>Spotify</li>
+                    <li><img class="icon" src="/assets/img/facebook.svg">Facebook</li>
+                    <li><img class="icon" src="/assets/img/twitter.svg">Twitter</li>
+                    <li><img class="icon" src="/assets/img/instagram.svg">Instagram</li>
+                    <!-- <li>Soundcloud</li>
+                    <li>Bandcamp</li> -->
+                    <li><img class="icon" src="/assets/img/spotify.svg">Spotify</li>
+                    <li><img class="icon" src="/assets/img/youtube.svg">Spotify</li>
+                    <li><img class="icon" src="/assets/img/linkedin.svg">Spotify</li>
             </div>
         </div>
     </div>

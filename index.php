f19f0cfa58fd47128aed007214c3cf1e6eebe533
ekiidx <info@vuedesign.co>
2023-09-15 02:24:22 -0400

    icons added

--- index.php
+++ index.php
@@ -10,66 +10,18 @@
     </div>
 </section>
 
+<!-- Dashboard -->
 <section id="dashboard">
     <div class="container">
-        <div id="bio" class="box">
-            <div class="container">
-                <h2>GuyRo</h2>
 
-                <p>Irish-born, Spanish-based electronica DJ and producer, David, a.k.a. GuyRo got swept up by the electronica steam train back during it's early beginnings. Harking back to his younger years, with fond memories as a pre-teen, armed with just a Sony Walkman and a collection of acid house mix tapes in his backpack, it is fair to say not many discover their passions quite as early as GuyRo. It wasn't long into his teenage years before he'd develop an insatiable thirst for crate digging — flipping through record after record picking out the dancefloor gems that would further ignite his passion for music.</p>
-
-                <p>With an open mind and fierce curiosity for anything and everything electronic music-based, he has spent the past two decades soaking up a multitude of genres and styles ranging from Detroit techno, breaks, house, trance, drum & bass and hip-hop. Artists like 808 State, Blake Baxter, Public Enemy, Alex Reece, Photek and BT are just some of the musicians that went on to shape GuyRo into the tastemaker he is today.</p>
-
-                <p>Today, GuyRo focuses on pushing an infectious fusion of synth-laden techno and progressive house. With recent influences such as Danny Howells, LSG, Boston 168, Saeed & Palash, Sasha and Charlie May, you'll be hard-pressed to argue for a lack of diversity within his sets, which comprise of both his own productions and others. His tracks go from the deeper end of downtempo and breaks through to the tougher grooves of house and techno.</p>
-            </div>
-        </div>
-        
-        <div id="photos" class="box">
-            <div class="container">
-                <h2>Photos</h2>
-            
-                <div class="photo-grid">
-                    <a href=""><div class="photo photo-1"></div></a>
-                    <a href=""><div class="photo photo-2"></div></a>
-                    <a href=""><div class="photo photo-3"></div></a>
-                    <a href=""><div class="photo photo-4"></div></a>
-                    <a href=""><div class="photo photo-5"></div></a>
-                    <a href=""><div class="photo photo-6"></div></a>    
-                </div>
-            </div>
-        </div>
-
-        <div id="calendar" class="box">
-            <div class="container">
-                <h2>Calendar</h2>
-            </div>
-        </div>
-
-        <div id="music" class="box">
-            <div class="container">
-                <h2>Music</h2>
-            </div>
-        </div>
-
-        <div id="press" class="box">
-            <div class="container">
-                <h2>Press</h2>
-
-                <div class="blog-single">
-                    <span class="title">The Underground Sound is Big - New Article on the New Movement</span>
-                    <p>The underground sound has never been the same since big underground parties started happening in the city a few summers ago. Back then the sound was hot and it's since gotten hotter.</p>
+        <!-- Socials -->
+        <div id="socials" class="box dark">
+            <div class="container d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <img class="icon" src="/assets/img/socials-dark.svg">
+                    <h2 class="fw-bold">Social Activity</h2>
                 </div>
 
-                <div class="blog-single">
-                    <span class="title">The Underground Sound is Big - New Article on the New Movement</span>
-                    <p>The underground sound has never been the same since big underground parties started happening in the city a few summers ago. Back then the sound was hot and it's since gotten hotter.</p>
-                </div>
-            </div>
-        </div>
-
-        <div id="socials" class="box">
-            <div class="container">
-                <h2>Social Activity</h2>
                 <div class="row">
                     <div class="col-6">
                         <span class="d-block">Facebook: 2323</span>
@@ -85,15 +37,139 @@
             </div>
         </div>
 
-        <div id="videos" class="box">
-            <div class="container">
-                <h2>Videos</h2>
+        <!-- Bio -->
+        <div id="bio" class="box">
+            <div class="container d-flex flex-column">
+                <h2 class="fw-bold">GuyRo</h2>
+
+                <p>Irish-born, Spanish-based electronica DJ and producer, David, a.k.a. GuyRo got swept up by the electronica steam train back during it's early beginnings. Harking back to his younger years, with fond memories as a pre-teen, armed with just a Sony Walkman and a collection of acid house mix tapes in his backpack, it is fair to say not many discover their passions quite as early as GuyRo. It wasn't long into his teenage years before he'd develop an insatiable thirst for crate digging — flipping through record after record picking out the dancefloor gems that would further ignite his passion for music.</p>
+
+                <p>With an open mind and fierce curiosity for anything and everything electronic music-based, he has spent the past two decades soaking up a multitude of genres and styles ranging from Detroit techno, breaks, house, trance, drum & bass and hip-hop. Artists like 808 State, Blake Baxter, Public Enemy, Alex Reece, Photek and BT are just some of the musicians that went on to shape GuyRo into the tastemaker he is today.</p>
+
+                <p>Today, GuyRo focuses on pushing an infectious fusion of synth-laden techno and progressive house. With recent influences such as Danny Howells, LSG, Boston 168, Saeed & Palash, Sasha and Charlie May, you'll be hard-pressed to argue for a lack of diversity within his sets, which comprise of both his own productions and others. His tracks go from the deeper end of downtempo and breaks through to the tougher grooves of house and techno.</p>
             </div>
         </div>
 
-        <div id="links" class="box">
-            <div class="container">
-                <h2>Links</h2>
+        <div id="calendar" class="box">
+            <div class="container d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <img class="icon" src="/assets/img/calendar-light.svg">
+                    <h2 class="fw-bold">Calendar</h2>
+                </div>
+
+                <div class="d-flex justify-content-between">
+                    <div class="photo photo-5"></div>
+                    <div class="date d-flex flex-column">
+                        <span class="fw-bold lh-1">23</span>
+                        <span class="fw-bold">May</span>
+                    </div>
+                    <div class="venue">
+                        <span class="fw-bold">House of Blues</span>
+                    </div>
+                    <div class="location">
+                        <span class="fw-bold">Cleveland, OH</span>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-between">
+                    <div class="photo photo-3"></div>
+                    <div class="date d-flex flex-column">
+                        <span class="fw-bold lh-1">23</span>
+                        <span class="fw-bold">May</span>
+                    </div>
+                    <div class="venue">
+                        <span class="fw-bold">House of Blues</span>
+                    </div>
+                    <div class="location">
+                        <span class="fw-bold">Cleveland, OH</span>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-between">
+                    <div class="photo photo-1"></div>
+                    <div class="date d-flex flex-column">
+                        <span class="fw-bold lh-1">23</span>
+                        <span class="fw-bold">May</span>
+                    </div>
+                    <div class="venue">
+                        <span class="fw-bold">House of Blues</span>
+                    </div>
+                    <div class="location">
+                        <span class="fw-bold">Cleveland, OH</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        
+        <div id="photos" class="box">
+            <div class="container d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <img class="icon" src="/assets/img/photos-light.svg">
+                    <h2 class="fw-bold">Photos</h2>
+                </div>
+            
+                <div class="photo-grid">
+                    <a href=""><div class="photo photo-1"></div></a>
+                    <a href=""><div class="photo photo-2"></div></a>
+                    <a href=""><div class="photo photo-3"></div></a>
+                    <a href=""><div class="photo photo-4"></div></a>
+                    <a href=""><div class="photo photo-5"></div></a>
+                    <a href=""><div class="photo photo-6"></div></a>    
+                </div>
+            </div>
+        </div>
+
+        
+
+        <div id="music" class="box">
+            <div class="container d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <img class="icon" src="/assets/img/music-light.svg">
+                    <h2 class="fw-bold">Music</h2>
+                </div>
+            </div>
+        </div>
+
+        <div id="press" class="box">
+            <div class="container d-flex flex-column">
+                <h2 class="fw-bold">Press</h2>
+
+                <div class="blog-single">
+                    <span class="title lh-1 fw-bold"><a href="">The Underground Sound is Big - New Article on the New Movement</a></span>
+                    <p>The underground sound has never been the same since big underground parties started happening in the city a few summers ago. Back then the sound was hot and it's since gotten hotter.</p>
+                </div>
+
+                <div class="blog-single">
+                    <span class="title lh-1 fw-bold"><a href="">The Underground Sound is Big - New Article on the New Movement</a></span>
+                    <p>The underground sound has never been the same since big underground parties started happening in the city a few summers ago. Back then the sound was hot and it's since gotten hotter.</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="videos" class="box">
+            <div class="container d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <img class="icon" src="/assets/img/videos-light.svg">
+                    <h2 class="fw-bold">Videos</h2>
+                </div>
+            </div>
+        </div>
+
+        <div id="links" class="box dark">
+            <div class="container d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <img class="icon" src="/assets/img/links-dark.svg">
+                    <h2 class="fw-bold">Links</h2>
+                </div>
+
+                <ul>
+                    <li>facebook.com</li>
+                    <li>ra.com</li>
+                    <li>soundcloud.com</li>
+                    <li>linktree</li>
+                    <li>youtube.com</li>
+                </ul>
             </div>
         </div>
     </div>

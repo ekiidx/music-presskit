41f4bb12b199b918149c1c7522f61798f3d99215
ekiidx <info@vuedesign.co>
2023-09-14 23:53:06 -0400

    compressed images

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 
 	<!--Bootstrap-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
+	
 	<!-- Fonts -->
 	<!--<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- index.php
+++ index.php
@@ -10,10 +10,10 @@
     </div>
 </section>
 
-<section id="bio">
+<section id="dashboard">
     <div class="container">
-        <div class="row">
-            <div class="col-6">
+        <div id="bio" class="box">
+            <div class="container">
                 <h2>GuyRo</h2>
 
                 <p>Irish-born, Spanish-based electronica DJ and producer, David, a.k.a. GuyRo got swept up by the electronica steam train back during it's early beginnings. Harking back to his younger years, with fond memories as a pre-teen, armed with just a Sony Walkman and a collection of acid house mix tapes in his backpack, it is fair to say not many discover their passions quite as early as GuyRo. It wasn't long into his teenage years before he'd develop an insatiable thirst for crate digging — flipping through record after record picking out the dancefloor gems that would further ignite his passion for music.</p>
@@ -22,86 +22,98 @@
 
                 <p>Today, GuyRo focuses on pushing an infectious fusion of synth-laden techno and progressive house. With recent influences such as Danny Howells, LSG, Boston 168, Saeed & Palash, Sasha and Charlie May, you'll be hard-pressed to argue for a lack of diversity within his sets, which comprise of both his own productions and others. His tracks go from the deeper end of downtempo and breaks through to the tougher grooves of house and techno.</p>
             </div>
-            <div class="col-6">
-                <section id="photos">
-                    <h2>Photos</h2>
-                    
-                    <div class="row">
-                        <div class="col-4"><img src=""></div>
-                        <div class="col-4"><img src=""></div>
-                        <div class="col-4"><img src=""></div>
-                        <div class="col-4"><img src=""></div>
-                        <div class="col-4"><img src=""></div>
-                        <div class="col-4"><img src=""></div>
-                    </div>
-                </section>
+        </div>
+        
+        <div id="photos" class="box">
+            <div class="container">
+                <h2>Photos</h2>
+            
+                <div class="photo-grid">
+                    <a href=""><div class="photo photo-1"></div></a>
+                    <a href=""><div class="photo photo-2"></div></a>
+                    <a href=""><div class="photo photo-3"></div></a>
+                    <a href=""><div class="photo photo-4"></div></a>
+                    <a href=""><div class="photo photo-5"></div></a>
+                    <a href=""><div class="photo photo-6"></div></a>    
+                </div>
             </div>
         </div>
-    </div>
-</section>
 
-<section id="dashboard">
-    <div class="container">
-        <div class="row">
-
-        <div id="calendar" class="col-6">
+        <div id="calendar" class="box">
             <div class="container">
                 <h2>Calendar</h2>
             </div>
         </div>
 
-        <div id="music" class="col-6">
+        <div id="music" class="box">
             <div class="container">
                 <h2>Music</h2>
             </div>
         </div>
 
-        <div id="press" class="col-6">
+        <div id="press" class="box">
             <div class="container">
                 <h2>Press</h2>
+
+                <div class="blog-single">
+                    <span class="title">The Underground Sound is Big - New Article on the New Movement</span>
+                    <p>The underground sound has never been the same since big underground parties started happening in the city a few summers ago. Back then the sound was hot and it's since gotten hotter.</p>
+                </div>
+
+                <div class="blog-single">
+                    <span class="title">The Underground Sound is Big - New Article on the New Movement</span>
+                    <p>The underground sound has never been the same since big underground parties started happening in the city a few summers ago. Back then the sound was hot and it's since gotten hotter.</p>
+                </div>
             </div>
         </div>
 
-        <div id="socials" class="col-6">
+        <div id="socials" class="box">
             <div class="container">
                 <h2>Social Activity</h2>
-                    <div class="row">
-                        <div class="col-6">
-                            <span class="d-block">Facebook: 2323</span>
-                            <span class="d-block">YouTube: 150</span>
-                            <span class="d-block">Soundcloud: 2399</span>
-                            <span class="d-block">Spotify: 11252</span>
-                        </div>
+                <div class="row">
+                    <div class="col-6">
+                        <span class="d-block">Facebook: 2323</span>
+                        <span class="d-block">YouTube: 150</span>
+                        <span class="d-block">Soundcloud: 2399</span>
+                        <span class="d-block">Spotify: 11252</span>
+                    </div>
+                    <div class="col-6">
+                        <span class="d-block">Total Followers: 3433</span>
+                        <span class="d-block">Total Actions: 3433</span>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div id="videos" class="col-6">
+        <div id="videos" class="box">
             <div class="container">
                 <h2>Videos</h2>
             </div>
-        </div>   
+        </div>
 
+        <div id="links" class="box">
+            <div class="container">
+                <h2>Links</h2>
+            </div>
+        </div>
     </div>
 </section>
 
-
-
-<section id="follow">
+<!-- <section id="contact">
     <div class="container">
         <div class="row">
             <div class="col-6">
-                <h2>Follow Me</h2>
+                <h2>Contact</h2>
             </div>
 
             <div class="col-6">
+                <h2>Follow Me</h2>
                 <ul>
                     <li><img class="icon" src="/assets/img/facebook.svg">Facebook</li>
                     <li><img class="icon" src="/assets/img/twitter.svg">Twitter</li>
                     <li><img class="icon" src="/assets/img/instagram.svg">Instagram</li>
-                    <!-- <li>Soundcloud</li>
-                    <li>Bandcamp</li> -->
+                     <li>Soundcloud</li>
+                    <li>Bandcamp</li>
                     <li><img class="icon" src="/assets/img/spotify.svg">Spotify</li>
                     <li><img class="icon" src="/assets/img/youtube.svg">Spotify</li>
                     <li><img class="icon" src="/assets/img/linkedin.svg">Spotify</li>
@@ -109,16 +121,6 @@
             </div>
         </div>
     </div>
-</section>
-
-<section id="contact">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-               <h2>Contact</h2>
-            </div>
-        </div>
-    </div>
-</section>
+</section> -->
 
 <?php include 'footer.php';
